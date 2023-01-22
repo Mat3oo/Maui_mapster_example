e69c396fe68c3a1a7a82dc2f9e5dae52c76cc12f
mat3o <matlagit@outlook.com>
2023-01-22 01:36:03 +0100

    add models library project

---
+++ Models/DTOs.cs
@@ -0,0 +1,12 @@
+﻿namespace Models;
+
+public class ParentDTO
+{
+    public string? Id { get; set; }
+    public ICollection<ChildDTO>? Childrens { get; set; }
+}
+
+public class ChildDTO
+{
+    public string? ChildId { get; set; }
+}
---
+++ Models/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace Models;
+
+public class Parent
+{
+    public string? Id { get; set; }
+
+    public ICollection<Child>? Childrens { get; set; }
+}
+
+public class Child
+{
+    public string? ChildId { get; set; }
+}
