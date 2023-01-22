ed5d950c319af102ee20a43c7905ecf79f0d227a
mat3o <matlagit@outlook.com>
2023-01-22 01:42:01 +0100

    add mapping exception example

--- mac_mapster/Main.cs
+++ mac_mapster/Main.cs
@@ -1,4 +1,20 @@
-﻿using mapsterTest;
+﻿using Mapster;
+using mapsterTest;
+using Models;
+
+try
+{
+    var parent1  = new Parent() { Id = "Parent1", Childrens = new List<Child>() { new Child() { ChildId = "Child1" } } };
+    var parent2 = new Parent() { Id = "Parent2", Childrens = new List<Child>() { new Child() { ChildId = "Child2" } } };
+
+    var parentList = new List<Parent>() { parent1, parent2 };
+
+    var mapped = parentList.Adapt<ICollection<ParentDTO>>();
+}
+catch (Exception ex)
+{
+    throw;
+}
 
 // This is the main entry point of the application.
 // If you want to use a different Application Delegate class from "AppDelegate"
--- maui_mapster/MauiProgram.cs
+++ maui_mapster/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Models;
+using Mapster;
 
 namespace maui_mapster;
 
@@ -6,6 +8,20 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
+        try
+        {
+            var parent1 = new Parent() { Id = "Parent1", Childrens = new List<Child>() { new Child() { ChildId = "Child1" } } };
+            var parent2 = new Parent() { Id = "Parent2", Childrens = new List<Child>() { new Child() { ChildId = "Child2" } } };
+
+            var parentList = new List<Parent>() { parent1, parent2 };
+
+            var mapped = parentList.Adapt<ICollection<ParentDTO>>();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
